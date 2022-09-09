c549f09f4f8e6ff50a9dec1db731917182e27e23
DValentinaBejarano <dvbejarano28@misena.edu.co>
2022-09-09 01:09:01 -0500

    Avance estilos para las páginas

--- js/Ejercicio4.js
+++ js/Ejercicio4.js
@@ -1,3 +1,4 @@
+//Interfaz
 let toggle = document.querySelector('.toggle')
 let navigation = document.querySelector('.navigation')
 let main = document.querySelector('.main')
@@ -15,3 +16,49 @@ function activeLink(){
 }
 list.forEach((item)=>
 item.addEventListener('mouseover', activeLink))
+
+/*Sección ejercicios
+const consultar = document.getElementById('consultar');
+
+consultar.addEventListener('click', function () {
+    const opcionN = document.getElementById('opcionN').value;
+    const opcionA = document.getElementById('opcionA').value;
+    const opcionAc = document.getElementById('opcionAc').value;
+    parseInt(opcionN, opcionA, opcionAc);
+
+    //Tatiana Cabrera
+        if (opcionN == 1 && opcionA == 6 && opcionAc == 11) {
+        alert(`Respuesta correcta`);
+        }else{
+            alert(`Respuesta incorrecta`);
+        }
+
+    //Carolina Forero  
+        if (opcionN == 2 && opcionA == 7 && opcionAc == 12) {
+        alert(`Respuesta correcta`);
+        }else{
+            alert(`Respuesta incorrecta`);
+        }
+    
+    //Yaneth Castillo
+        if (opcionN == 3 && opcionA == 8 && opcionAc == 13) {
+        alert(`Respuesta correcta`);
+        }else{
+            alert(`Respuesta incorrecta`);
+        }
+        
+    //Fernando Galindo
+        if (opcionN == 4 && opcionA == 9 && opcionAc == 14) {
+        alert(`Respuesta correcta`);
+        }else{
+            alert(`Respuesta incorrecta`);
+        }
+
+    //Cristian Buitrago
+         if (opcionN == 5 && opcionA == 10 && opcionAc == 15) {
+        alert(`Respuesta correcta`);
+        }else{
+            alert(`Respuesta incorrecta`);
+        }
+
+})*/
